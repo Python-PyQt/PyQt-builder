518ceac3901336574924509f2114ddb74e506619
Phil Thompson <phil@riverbankcomputing.com>
2019-08-17 16:30:22 +0100

    Set the generator for the .dist-info directory.

--- PyQt5_builder/qmake_builder.py
+++ PyQt5_builder/qmake_builder.py
@@ -105,10 +105,10 @@ SUBDIRS = {}
 
         inventory.close()
 
-        pro.write('''distinfo.extra = sip5-distinfo --prefix \\"$(INSTALL_ROOT)\\" --inventory {} {}
+        pro.write('''distinfo.extra = sip5-distinfo --generator {} --prefix \\"$(INSTALL_ROOT)\\" --inventory {} {}
 distinfo.path = {}/{}
 INSTALLS += distinfo
-'''.format(inventory_fn, project.get_distinfo_name(target_dir).replace('\\', '/'), target_dir.replace('\\', '/'), project.name))
+'''.format(os.path.basename(sys.argv[0]), inventory_fn, project.get_distinfo_name(target_dir).replace('\\', '/'), target_dir.replace('\\', '/'), project.name))
 
         pro.close()
 

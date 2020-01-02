f7cab8bd1b61eb91112c2dcdf7c59b0c616f9277
Phil Thompson <phil@riverbankcomputing.com>
2020-01-02 13:14:26 +0000

    SIP v5.1 is required.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, Riverbank Computing Limited
+# Copyright (c) 2020, Riverbank Computing Limited
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -51,7 +51,7 @@ setup(
         version=version,
         license='BSD',
         python_requires='>=3.5',
-        install_requires=['sip >=5, <6'],
+        install_requires=['sip >=5.1, <6'],
         packages=find_packages(),
         package_data={
             'pyqtbuild.bundle': ['dlls/*/*'],
